d59a3fc9034f5964cdf03c0ecf7a7ad10380fbb0
Behzad <behzodxidirov@gmail.com>
2020-07-02 20:06:41 +0500

    oxirgisi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProductList from "./components/ProductList";
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+
 import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
